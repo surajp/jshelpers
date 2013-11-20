a1abd5919c7eb7a7a19be22bba411f5105a919bf
Suraj Pillai <85.suraj@gmail.com>
2013-11-19 23:22:01 -0500

    Fixed indentation

--- jQueryTableSort.js
+++ jQueryTableSort.js
@@ -1,19 +1,19 @@
-$('table#sortable  thead  th.clickable').click(function(){
-      var asc=$(this).hasClass('asc');
-          $(this).toggleClass('asc');
-              var index=$(this).index();
-                  var tbody=$(this).parent('tr').parent('thead').next('tbody');
-                      var rows=tbody.children('tr');
-                          rows.sort(function(a,b){
-                                    var textA=$('td:eq('+index+')',a).text();
-                                              var textB=$('td:eq('+index+')',b).text();
-                                                        if(!isNaN(textA)&&!isNaN(textB)){
-                                                                      var res= parseInt(textA) > parseInt(textB);
-                                                                                  return asc ? res: !res;
-                                                                                          }else
-                                                                                                      return asc ? (textA > textB): (textB > textA);
-                                                                                                          });
-                                                  rows.each(function(){
-                                                            tbody.append($(this));
-                                                                });
-                                                  });
+$('table#sortable thead th.clickable').click(function(){
+    var asc=$(this).hasClass('asc');
+    $(this).toggleClass('asc');
+    var index=$(this).index();
+    var tbody=$(this).parent('tr').parent('thead').next('tbody');
+    var rows=tbody.children('tr');
+    rows.sort(function(a,b){
+          var textA=$('td:eq('+index+')',a).text();
+          var textB=$('td:eq('+index+')',b).text();
+          if(!isNaN(textA)&&!isNaN(textB)){
+              var res= parseInt(textA) > parseInt(textB);
+              return asc ? res: !res;
+          }else
+              return asc ? (textA > textB): (textB > textA);
+          });
+          rows.each(function(){
+          tbody.append($(this));
+          });
+});
